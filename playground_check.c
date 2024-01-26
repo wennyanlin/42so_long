ea8c0d2d7f52891644f31c4a2b48955169f0ae9d
Wenyan Lin <wennyan.lin@gmail.com>
2024-01-26 13:04:34 +0800

    [#6] split out playground util functions

--- playground_check.c
+++ playground_check.c
@@ -1,54 +1,5 @@
 #include "so_long.h"
 
-char **buildPlayground(int height, int width)
-{
-	char **playground;
-	int i = 0;
-	int	j = 0;
-
-	playground = malloc((sizeof(char*)) * (height + 1));
-	if (!playground)
-		return (NULL);
-	while (i < height)
-	{
-        playground[i] = malloc(sizeof(char) * (width + 1));
-		if (!playground[i])
-			return (NULL);
-
-		if (i == 0 || i == (height - 1))
-		{
-			j = 0;
-			while (j < width)
-				playground[i][j++] = '1';
-		}
-		else
-		{
-			playground[i][0] = '1';
-			playground[i][width - 1] = '1';
-			j = 1;
-			while (j != 0 && j != (width - 1))
-			{
-				playground[i][j++] = '0';
-			}
-		}
-		playground[i][width] = '\0';
-		i++;
-	}
-	return (playground);
-}
-
-void write_playground(int fd, char **playground, int width)
-{
-	int i = 0;
-
-	while(playground[i])
-	{
-		write(fd, playground[i], width);
-		write(fd, "\n", 1);
-		i++;
-	}
-}
-
 int	is_playground_shape_valid(char **arr)
 {
 	int	i;
@@ -74,15 +25,6 @@ int	is_playground_shape_valid(char **arr)
 }
 
 
-// char	*read_file(char *filepath)
-// {
-// 	int	fd;
-
-// 	fd = open(filepath, O_CREAT | O_RDWR);
-// 	if (fd == -1)
-// 		return (NULL);
-
-// }
 
 int	main()
 {
