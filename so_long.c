ac8e1e14e566ec48450722cdfedf48a5baf748a9
Wenyan Lin <wennyan.lin@gmail.com>
2024-01-29 10:57:36 +0800

    [#10] reading command character

--- so_long.c
+++ so_long.c
@@ -1,15 +1,32 @@
 #include "so_long.h"
+//handle input is more than 1 characters
+char	read_direction_command(int input_fd)
+{
+	char	*buffer;
+	char	command;
+
+	buffer = malloc(sizeof(char) * 2);
+	if (!buffer)
+		return (0);
+	read(input_fd, buffer, 1);
+	printf("command: %c\n", buffer[0]);
+	command = buffer[0];
+	free(buffer);
+	return (command);
+}
 
 int	main()
 {
 	// test_read();
 	// test_string_utils();
 	// test_split();
-	test_playground_check();
+	// test_playground_check();
+	read_direction_command(STDIN_FILENO);
 
 	/**
 	 * read the map
 	 * validate the map
+	 *	- a valid route to exit
 	 * play the game
 	 * 	 - read the direction command
 	 * 	 - validate the direction command
@@ -20,6 +37,5 @@ int	main()
 	 * 	 - if exit and no collectibles left, end the game
 	 * 	 - if exit and there are collectible, you can't leave!
 	*/
-
 	return (0);
 }
